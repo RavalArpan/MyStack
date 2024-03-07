fd8a66b72e25a1ebc5322d87042ef32259fb46d2
RavalArpan <arpanraval17@gmail.com>
2024-03-06 19:23:43 -0500

    written test cases to check 1. push, pop 2. multiple push, pop 3. pop on empty 4. check for empty stack 5. size of stack after push, pop

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -4,6 +4,8 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
+import java.util.EmptyStackException;
+
 /**
  * Unit test for simple App.
  */
@@ -15,9 +17,59 @@ public class AppTest
      *
      * @param testName name of the test case
      */
+
+
     public AppTest( String testName )
     {
         super( testName );
+    }
+    public void testPushAndPop(){
+        MyStack<Integer> stack = new MyStack<>();
+        assertTrue(stack.isEmpty());
+
+        stack.push(5);
+        assertFalse(stack.isEmpty());
+        assertEquals(5,(int)stack.pop());
+        assertTrue(stack.isEmpty());
+    }
+
+    public void testMultiplePushAndPop() {
+        MyStack<Integer> stack = new MyStack<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        assertEquals(3, (int)stack.pop());
+        assertEquals(2, (int)stack.pop());
+        assertEquals(1, (int)stack.pop());
+        assertTrue(stack.isEmpty());
+    }
+
+    public void testPopOnEmptyStack() {
+        MyStack<Double> stack = new MyStack<>();
+        assertTrue(stack.isEmpty());
+        try {
+            stack.pop(); // This should throw EmptyStackException
+            fail("Expected EmptyStackException");
+        } catch (EmptyStackException e) {
+            // Pass
+        }
+    }
+
+    public void testEmptyStack() {
+        MyStack<Boolean> stack = new MyStack<>();
+        assertTrue(stack.isEmpty());
+        assertEquals(0, stack.size());
+    }
+
+    public void testSizeAfterPushAndPop() {
+        MyStack<Integer> stack = new MyStack<>();
+        stack.push(1);
+        stack.push(2);
+        assertEquals(2, stack.size());
+        stack.pop();
+        assertEquals(1, stack.size());
+        stack.pop();
+        assertEquals(0, stack.size());
     }
 
     /**
